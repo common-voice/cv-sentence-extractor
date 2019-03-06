3af51b68b816c7f4a029438c107a6309c582e363
Florian Merz <flomerz@gmail.com>
2019-03-06 23:21:18 +0100

    first

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+extern crate bzip2;
+extern crate parse_mediawiki_dump;
+extern crate parse_wiki_text;
+
+use parse_wiki_text::{Configuration, Node};
+
+fn main() {
+    let mut args = std::env::args();
+    if args.len() != 2 {
+        eprintln!("invalid use");
+        std::process::exit(1);
+    }
+    let path = args.nth(1).unwrap();
+    let file = match std::fs::File::open(&path) {
+        Err(error) => {
+            eprintln!("Failed to open input file: {}", error);
+            std::process::exit(1);
+        }
+        Ok(file) => std::io::BufReader::new(file),
+    };
+    if path.ends_with(".bz2") {
+        parse(std::io::BufReader::new(bzip2::bufread::BzDecoder::new(
+            file,
+        )));
+    } else {
+        parse(file);
+    }
+}
+
+fn parse(source: impl std::io::BufRead) {
+    let config = Configuration::default();
+    for result in parse_mediawiki_dump::parse(source) {
+        match result {
+            Err(error) => {
+                eprintln!("Error: {}", error);
+                std::process::exit(1);
+            }
+            Ok(page) => println!("{}", page.text),
+        }
+    }
+}
+
+fn parse_text(text: &str, config: &Configuration) {
+    let result = config.parse(text);
+    if result.warnings.is_empty() {
+        for node in result.nodes {
+            if let Node::Text{ value, .. } = node {
+                println!("{}", value);
+            }
+        }
+    }
+}
