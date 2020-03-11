bc4762fc6a8551f41dee492ff1d1704b526e3804
Michael Kohler <me@michaelkohler.info>
2020-03-11 22:41:42 +0100

    Read out all files and iterate over them

--- scripts/extraction.sh
+++ scripts/extraction.sh
@@ -11,50 +11,7 @@ WIKI_EXTRACTOR_PATH="$WORKSPACE/WikiExtractor.py"
 EXTRACTED_TEXT_PATH="$WORKSPACE/text"
 OUTPUT_PATH="$WORKSPACE/output"
 EXTRACTED_SENTENCES_PATH="$OUTPUT_PATH/extraction-sample.txt"
-
 mkdir -p $OUTPUT_PATH
-
-if [ $TYPE == "sample" ]; then
-  echo "Files created: $FILES_CREATED"
-  echo "Files updated: $FILES_UPDATED"
-  echo "Analyzing first rule file changed.."
-  ALL_FILES="$FILES_CREATED $FILES_UPDATED"
-  FIRST_CHANGED_RULES_FILE=( $(echo $ALL_FILES | grep -o 'src/rules/.*' || [[ $? == 1 ]]) )
-
-  if [ ${#FIRST_CHANGED_RULES_FILE[@]} == 0 ]; then
-    echo "Nothing to be done here.."
-    echo "" > $EXTRACTED_SENTENCES_PATH
-    exit 0
-  fi
-
-  LANGUAGE_FILE_NAME=${FIRST_CHANGED_RULES_FILE/src\/rules\//""}
-  LANGUAGE_FILE_NAME=${LANGUAGE_FILE_NAME/disallowed_words\//""}
-  LANGUAGE=${LANGUAGE_FILE_NAME/.toml/""}
-  LANGUAGE_CODE=${LANGUAGE/.txt/""}
-elif [ $TYPE == "full" ]; then
-  echo "Commit: $COMMIT_MESSAGE"
-  EXTRACTION_OPTION=$(echo $COMMIT_MESSAGE | grep -o -e '--full-extraction=.*$' || [[ $? == 1 ]])
-  LANGUAGE_CODE=${EXTRACTION_OPTION/"--full-extraction="/""}
-fi
-
-echo "Determined that we should run an export for $LANGUAGE_CODE"
-
-echo "Getting WikiExtractor"
-curl $WIKI_EXTRACTOR_URL > $WIKI_EXTRACTOR_PATH
-
-echo "Downloading Dump Listing..."
-DUMP_BASE_PATH="https://dumps.wikimedia.org/${LANGUAGE_CODE}wiki/latest/"
-curl $DUMP_BASE_PATH > listing.html
-
-echo "Searching for correct file..."
-ARCHIVE_FILE_NAME_MATCHES=$(grep -o -e 'wiki-latest-pages-articles-multistream1.xml.*bz2' < listing.html || [[ $? == 1 ]])
-if [ -z $ARCHIVE_FILE_NAME_MATCHES ]; then
-  ARCHIVE_FILE_NAME_MATCHES=$(grep -o -e 'wiki-latest-pages-articles-multistream.xml.bz2' < listing.html)
-fi
-rm listing.html
-
-ARCHIVE_FILE_NAME=$(echo $ARCHIVE_FILE_NAME_MATCHES | head -n1 | awk '{print $1;}')
-
 
 function dump {
   DUMP_URL="${DUMP_BASE_PATH}${LANGUAGE_CODE}$1"
@@ -91,10 +48,51 @@ function cleanup {
   rm -rf $EXTRACTED_TEXT_PATH
 }
 
-function main {
-  dump $ARCHIVE_FILE_NAME
-  extract
-  cleanup
-}
 
-main
+if [ $TYPE == "sample" ]; then
+  echo "Files created: $FILES_CREATED"
+  echo "Files updated: $FILES_UPDATED"
+  echo "Analyzing first rule file changed.."
+  ALL_FILES="$FILES_CREATED $FILES_UPDATED"
+  FIRST_CHANGED_RULES_FILE=( $(echo $ALL_FILES | grep -o 'src/rules/.*' || [[ $? == 1 ]]) )
+
+  if [ ${#FIRST_CHANGED_RULES_FILE[@]} == 0 ]; then
+    echo "Nothing to be done here.."
+    echo "" > $EXTRACTED_SENTENCES_PATH
+    exit 0
+  fi
+
+  LANGUAGE_FILE_NAME=${FIRST_CHANGED_RULES_FILE/src\/rules\//""}
+  LANGUAGE_FILE_NAME=${LANGUAGE_FILE_NAME/disallowed_words\//""}
+  LANGUAGE=${LANGUAGE_FILE_NAME/.toml/""}
+  LANGUAGE_CODE=${LANGUAGE/.txt/""}
+elif [ $TYPE == "full" ]; then
+  echo "Commit: $COMMIT_MESSAGE"
+  EXTRACTION_OPTION=$(echo $COMMIT_MESSAGE | grep -o -e '--full-extraction=.*$' || [[ $? == 1 ]])
+  LANGUAGE_CODE=${EXTRACTION_OPTION/"--full-extraction="/""} # TODO: make this prettier by only returing matched language
+fi
+
+echo "Determined that we should run an export for $LANGUAGE_CODE"
+
+echo "Getting WikiExtractor"
+curl $WIKI_EXTRACTOR_URL > $WIKI_EXTRACTOR_PATH
+
+echo "Downloading Dump Listing..."
+DUMP_BASE_PATH="https://dumps.wikimedia.org/${LANGUAGE_CODE}wiki/latest/"
+curl $DUMP_BASE_PATH > listing.html
+
+echo "Searching for correct files..."
+ARCHIVE_FILE_NAME_MATCHES=($(grep -o  -P -e 'wiki-latest-pages-articles-multistream\d*.xml-.*bz2"' < listing.html || [[ $? == 1 ]]))
+if [ ${#ARCHIVE_FILE_NAME_MATCHES[@]} == 0 ]; then
+  ARCHIVE_FILE_NAME_MATCHES=($(grep -o  -P -e 'wiki-latest-pages-articles-multistream.xml.bz2"' < listing.html))
+fi
+rm listing.html
+
+for archive in "${ARCHIVE_FILE_NAME_MATCHES[@]}"
+do
+    ARCHIVE_FILE_NAME=${archive/%?/}
+    echo "Starting extraction for ... $ARCHIVE_FILE_NAME" # TODO: make this prettier and not even return " when grepping...
+    dump $ARCHIVE_FILE_NAME
+    extract
+    cleanup
+done
