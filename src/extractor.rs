ff83ef8efd0b6e4da4a8292cff072ed98875c508
Antonio Yang <yanganto@gmail.com>
2020-06-01 22:47:06 +0800

    Update mandarin branch with more features
    * add translate option
      - with `--trans` or `-t` the traditional Chinese will translate to simplify Chinese
      - default will not translate<F10>
    
    * add sutiable length feature, aux symbols, fix test
      - add the longest length feature
      - add the shortest length feature
      - add patch aux symbol feature
    
    * fix test case
    * update app info
      - add maintainer and email

--- src/extractor.rs
+++ src/extractor.rs
@@ -1,22 +1,55 @@
+use std::default::Default;
+
 use regex::Regex;
+use std::fmt::Debug;
 
 use crate::character_map::CHARACTER_MAP;
 use crate::standard_characters::STANDARD_CHARACTERS;
 
-static TERMINAL_PUNCTUATIONS: [char; 3] = ['。', '？', '！'];
+static TERMINAL_PUNCTUATIONS: [char; 4] = ['。', '？', '！', '\n'];
 static PUNCTUATIONS: [char; 37] = [
-    '"', '"', '、', '‧', '—', '—', '—', '～', '“', '”', '；', '·', '：', '‘',
-    '•', '─', '兀', '∶', '∧', '∨', '，', '、', '．', '；', '：', '＃', '＆',
-    '＊', '＋', '－', '＜', '＞', '＝', '＄', '％', '＠', '，',
+    '"', '"', '、', '‧', '—', '—', '—', '～', '“', '”', '；', '·', '：', '‘', '•', '─', '兀', '∶',
+    '∧', '∨', '，', '、', '．', '；', '：', '＃', '＆', '＊', '＋', '－', '＜', '＞', '＝', '＄',
+    '％', '＠', '，',
 ];
 
+#[derive(Debug)]
 pub struct SentenceExtractor {
     text: String,
+    /// Boolean option for translate words from traditional Chinese into simplify Chinese
+    translate: bool,
+    auxiliary_symbols: Vec<char>,
+    shortest_length: usize,
+    longest_length: usize,
+    cut_with_auxiliary_symbols: bool,
+}
+
+impl Default for SentenceExtractor {
+    fn default() -> Self {
+        SentenceExtractor {
+            text: String::new(),
+            translate: true,
+            auxiliary_symbols: vec!['，', '：', '；', '。', '？', '！', '\n'],
+            shortest_length: 3,
+            longest_length: 38,
+            cut_with_auxiliary_symbols: false,
+        }
+    }
 }
 
 impl SentenceExtractor {
-    pub fn new(text: &str) -> SentenceExtractor {
+    /// New the Extractor with translate option for automatically translate words from traditional Chinese into
+    /// simplify Chinese
+    pub fn new_with_opt(
+        text: &str,
+        translate: bool,
+        shortest_length: usize,
+        longest_length: usize,
+        auxiliary_symbols: Vec<char>,
+    ) -> SentenceExtractor {
         let lines: Vec<&str> = text.lines().collect();
+        let mut merge_symbols = auxiliary_symbols;
+        merge_symbols.extend_from_slice(&TERMINAL_PUNCTUATIONS);
         SentenceExtractor {
             text: if lines.len() > 1 {
                 // skip disambiguation pages
@@ -29,6 +62,11 @@ impl SentenceExtractor {
             } else {
                 text.to_string()
             },
+            translate,
+            shortest_length,
+            longest_length,
+            auxiliary_symbols: merge_symbols,
+            ..Default::default()
         }
     }
 }
@@ -53,9 +91,16 @@ impl Iterator for SentenceExtractor {
             }
 
             let chars = self.text.chars().collect::<Vec<_>>();
-            let end_index = chars
-                .iter()
-                .position(|&c| TERMINAL_PUNCTUATIONS.contains(&c) || c == '\n');
+            let end_index = if self.cut_with_auxiliary_symbols {
+                self.cut_with_auxiliary_symbols = false;
+                chars
+                    .iter()
+                    .position(|&c| self.auxiliary_symbols.contains(&c))
+            } else {
+                chars
+                    .iter()
+                    .position(|&c| TERMINAL_PUNCTUATIONS.contains(&c))
+            };
             let index = end_index.unwrap_or(chars.len());
             let mut next_item = chars
                 .iter()
@@ -68,57 +113,34 @@ impl Iterator for SentenceExtractor {
                 .skip(index + (if end_index.is_some() { 1 } else { 0 }))
                 .collect::<String>();
 
+            // remove words in brackets
             next_item = PARANS.replace(&next_item, "").to_string();
-            next_item = next_item
-                .chars()
-                .map(|c| CHARACTER_MAP.get(&c).unwrap_or(&c).clone())
-                .collect();
 
+            // transalte words into simplify format
+            if self.translate {
+                next_item = next_item
+                    .chars()
+                    .map(|c| CHARACTER_MAP.get(&c).unwrap_or(&c).clone())
+                    .collect();
+            }
+            // adjust sentence in a suitable length
             let count = next_item.chars().count();
-            if is_invalid(&next_item)
-                || count < 3
-                || count > 38
-                || next_item.chars().any(|c| {
+            if count > self.longest_length {
+                self.cut_with_auxiliary_symbols = true;
+                continue;
+            } else if is_invalid(&next_item) || count < self.shortest_length {
+                continue;
+            } else if self.translate
+                && next_item.chars().any(|c| {
                     !TERMINAL_PUNCTUATIONS.contains(&c)
                         && !PUNCTUATIONS.contains(&c)
-                        && !STANDARD_CHARACTERS.contains(&c)
+                        && !STANDARD_CHARACTERS.contains(&c) // NOTE Standard characters only work for simplify chinese words
                 })
             {
                 continue;
             }
 
-            let abs_end = index + (if end_index.is_some() { 1 } else { 0 });
-            self.text = chars
-                .iter()
-                .skip(
-                    // skip over the next sentence, we don't want consecutive sentences
-                    abs_end
-                        + chars
-                            .iter()
-                            .skip(abs_end)
-                            .position(|&c| TERMINAL_PUNCTUATIONS.contains(&c) || c == '\n')
-                            .unwrap_or(0),
-                )
-                .collect::<String>();
-
-            if let Some(i) = end_index {
-                next_item.push(*chars.get(i).unwrap());
-            }
             return Some(next_item.trim().to_string());
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_split_after() {
-        let value = "唐王國。鹰号称铜。高等";
-        let mut iter = SentenceExtractor::new(value);
-        assert_eq!(iter.next().unwrap(), "鹰号称铜。");
-        assert_eq!(iter.next().unwrap(), "高等");
-        assert!(iter.next().is_none());
     }
 }
