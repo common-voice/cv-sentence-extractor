4318e7140b2db18fb48ab978e4dd684ba27907ab
Michael Kohler <me@michaelkohler.info>
2020-10-27 19:31:19 +0100

    Print command structure on error

--- src/app.rs
+++ src/app.rs
@@ -12,6 +12,18 @@ where
     I: IntoIterator<Item = T>,
     T: Into<OsString> + Clone,
 {
+    let language_argument = Arg::with_name("language")
+                                .short("l")
+                                .long("lang")
+                                .takes_value(true)
+                                .number_of_values(1)
+                                .help("language as identified by ISO code - for example en, de, es");
+    let no_check_argument = Arg::with_name("no_check")
+                                .short("n")
+                                .long("no_check")
+                                .takes_value(false)
+                                .help("output all the sentences without verification");
+
     App::new("common_voice_sentence_collector")
         .about("Common Voice Sentence Extraction Helper")
         .version(VERSION)
@@ -19,29 +31,16 @@ where
         .subcommand(
             SubCommand::with_name("extract")
                 .about("Extract sentences from Wikipedia dump extracts using WikiExtractor")
-                .arg(
-                    Arg::with_name("language")
-                        .short("l")
-                        .long("lang")
-                        .takes_value(true)
-                        .number_of_values(1)
-                        .help("language as identified by ISO code - for example en, de, es"),
-                )
+                .arg(language_argument)
+                .arg(no_check_argument)
                 .arg(
                     Arg::with_name("dir")
                         .short("d")
                         .long("dir")
                         .takes_value(true)
                         .number_of_values(1)
-                        .help("path to WikiExtractor folder"),
+                        .help("path to WikiExtractor folder")
                 )
-                .arg(
-                    Arg::with_name("no_check")
-                        .short("n")
-                        .long("no_check")
-                        .takes_value(false)
-                        .help("output all the sentences without verification")
-                ),
         )
         .get_matches_from(itr)
 }
@@ -58,7 +57,7 @@ where
 fn start(all_matches: ArgMatches) -> Result<(), String> {
     // Wikipedia
     if let Some(matches) = all_matches.subcommand_matches("extract") {
-        let config = Config {
+        let wikipedia_config = Config {
             language: String::from(matches.value_of("language").unwrap_or("en")),
             no_check: matches.is_present("no_check"),
             directory: String::from(matches.value_of("dir").unwrap_or_default()),
@@ -66,8 +65,9 @@ fn start(all_matches: ArgMatches) -> Result<(), String> {
             file_prefix: String::from("wiki_"),
         };
 
-        extract(config, load_wikiextractor)
+        extract(wikipedia_config, load_wikiextractor)
     } else {
-        Err(String::from("did we forget to add a subcommand?"))
+        println!("{}", all_matches.usage());
+        Err(String::from("Did you forget to add a subcommand?"))
     }
 }
