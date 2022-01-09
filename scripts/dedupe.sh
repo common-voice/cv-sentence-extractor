ab9d4c7d45744898d5730058e4b10d1588e61b65
Michael Kohler <me@michaelkohler.info>
2022-01-09 20:49:45 +0100

    Use temp file

--- scripts/dedupe.sh
+++ scripts/dedupe.sh
@@ -7,10 +7,11 @@ WORKSPACE=${GITHUB_WORKSPACE:-/tmp}
 OUTPUT_PATH="$WORKSPACE/output"
 FILE_PATH="$OUTPUT_PATH/$FILE_NAME"
 OUTPUT_FILE="$OUTPUT_PATH/$FILE_NAME"
+TEMP_FILE="$OUTPUT_PATH/temp.txt"
 
 if [[ -f "$FILE_PATH" ]]; then
   echo "Deduplicating $FILE_PATH"
-  sort -u $FILE_PATH > $OUTPUT_FILE
-  echo "Re-shuffling $OUTPUT_FILE"
-  shuf $OUTPUT_FILE > $OUTPUT_FILE
+  sort -u $FILE_PATH > $TEMP_FILE
+  echo "Re-shuffling $TEMP_FILE to $OUTPUT_FILE"
+  shuf $TEMP_FILE > $OUTPUT_FILE
 fi
